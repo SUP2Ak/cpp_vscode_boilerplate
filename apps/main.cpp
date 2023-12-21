85986ad584b5d7d3c02ae9aa27314328ebc95eb5
SUP2Ak <cormier.wesley@gmail.com>
2023-12-21 21:30:04 +0100

    init

--- apps/main.cpp
+++ apps/main.cpp
@@ -4,10 +4,14 @@
 #include <chrono>
 #include <thread>
 #include <conio.h>
+#include "test.h"
 
-int main()
+int main(int, char**)
 {
     std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"}; // initialize vector with string elements
+
+    std::string strGenerared = GenerateString("Hello World"); // call function from test.h
+    std::cout << strGenerared << std::endl; // print message
 
     for (const std::string& word : msg) // range-based for loop
     {
@@ -18,6 +22,7 @@ int main()
     std::cout << "Press any key to continue..." << std::endl; // print message
     char keyPressed = _getch(); // wait for user input
     std::cout << "You pressed: " << keyPressed << std::endl; // print message
+    
     
     for (int i = 5; i > 0; --i) // Loop to show countdown before closing
     {
---
+++ project/src/test.cpp
@@ -0,0 +1,6 @@
+#include <test.h>
+
+const std::string GenerateString(const std::string& str)
+{
+    return str + " <<== OMG THAT WORK";
+}
