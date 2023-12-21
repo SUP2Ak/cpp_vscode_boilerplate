51b57e8e4ba404bfd631dcb3ec0a0f5c60c59b14
SUP2Ak <cormier.wesley@gmail.com>
2023-12-21 20:00:07 +0100

    tweak(main): use more lib func

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
-using namespace std;
+#include <chrono>
+#include <thread>
+#include <conio.h>
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"}; // initialize vector with string elements
 
-    for (const string& word : msg)
+    for (const std::string& word : msg) // range-based for loop
     {
-        cout << word << " ";
+        std::cout << word << " "; // print each word followed by a space
     }
-    cout << endl;
-    return 0;
+
+    std::cout << std::endl; // print a new line
+    std::cout << "Press any key to continue..." << std::endl; // print message
+    char keyPressed = _getch(); // wait for user input
+    std::cout << "You pressed: " << keyPressed << std::endl; // print message
+    
+    for (int i = 5; i > 0; --i) // Loop to show countdown before closing
+    {
+        std::cout << "Closing in " << i << " seconds..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    return 0; // return 0 to indicate successful termination
 }
